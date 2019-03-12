3d8aa7b7ae403fe54c06355597aa25bee5a8b833
jinka <voiphandy@gmail.com>
2019-03-12 09:31:44 +0300

    Pushing to github

--- insta/tests.py
+++ insta/tests.py
@@ -5,7 +5,7 @@ class ImageTestClass(TestCase):
 
     # Set up method
     def setUp(self):
-        self.daudi= Image(image = 'image1', name='daudi',caption ='Caption 1', date_created ='2019 March, 11')
+        self.daudi= Image(image = 'image1', name='daudi',caption ='Caption 1',likes='1',user_id=1)
 
     # Testing  instance
     def test_instance(self):
@@ -13,6 +13,6 @@ class ImageTestClass(TestCase):
 
     # Testing Save Method
     def test_save_method(self):
-        self.james.save_image()
+        self.daudi.save_image()
         image = Image.objects.all()
         self.assertTrue(len(image) > 0)
