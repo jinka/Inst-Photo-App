13eaed1364d5a4e52110d7b7719aa6dd4d1e67ac
jinka <voiphandy@gmail.com>
2019-03-14 04:30:26 +0300

    Done test for save_image() for Image class

--- insta/tests.py
+++ insta/tests.py
@@ -1,18 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.db import models
 from .models import Image
 
-class ImageTestClass(TestCase):
+class ImageTestCase(TestCase):
 
     # Set up method
     def setUp(self):
-        self.daudi= Image(image = 'image1', name='daudi',caption ='Caption 1',likes=0)
+        self.user1 = User.objects.create(username='user1')
+        self.image=Image.objects.create(name="Image 1",caption="Caption 1", user=self.user1)        
+
+    def tearDown(self):
+        self.image.delete()
+        self.user1.delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
 
     # Testing  instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.daudi,Image))
+        self.assertTrue(isinstance(self.image,Image))
 
-    # Testing Save Method
+    # # Testing Save Method
     def test_save_method(self):
-        self.daudi.save_image()
+        self.image.save_image()
         image = Image.objects.all()
         self.assertTrue(len(image) > 0)
